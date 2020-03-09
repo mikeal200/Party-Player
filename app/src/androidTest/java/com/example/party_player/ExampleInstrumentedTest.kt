deb919217f8048d1d8bba797f30d2a5e29cc527f
Gabriel <kingnachos3456@yahoo.com>
2020-03-08 22:33:03 -0400

    Code Review

--- app/src/androidTest/java/com/example/party_player/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/party_player/ExampleInstrumentedTest.kt
@@ -17,7 +17,8 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
+        // Context of the app under test
+
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.party_player", appContext.packageName)
     }
