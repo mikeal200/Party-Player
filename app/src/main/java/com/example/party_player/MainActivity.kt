9aa63e29ec02d90230515d2e6427fcc9d43b4247
Michael Williams <willi6mc@mail.uc.edu>
2020-03-13 00:25:13 -0400

    got play stop and resume buttons to work

--- app/src/main/java/com/example/party_player/MainActivity.kt
+++ app/src/main/java/com/example/party_player/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.party_player
 
 import android.os.Bundle
-
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-
 import com.spotify.android.appremote.api.ConnectionParams
 import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.PlayerApi
 import com.spotify.android.appremote.api.SpotifyAppRemote
-
+import com.spotify.protocol.types.Empty
 import com.spotify.protocol.types.Track
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,32 +19,74 @@ class MainActivity : AppCompatActivity() {
     private val redirectUri = "com.example.party-player://callback"
     lateinit var spotifyAppRemote: SpotifyAppRemote
 
+
+    //need to fix lambda problem
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupViews()
+        setupListeners()
     }
 
-    override fun onStart() {
-        super.onStart()
-        val connectionParams = ConnectionParams.Builder(clientId)
-            .setRedirectUri(redirectUri)
-            .showAuthView(true)
-            .build()
-
-        SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
-            override fun onConnected(appRemote: SpotifyAppRemote) {
-                spotifyAppRemote = appRemote
-                Log.d("MainActivity", "Connected! Yay!")
-                // Now you can start interacting with App Remote
-                connected()
-                //spotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+    private fun setupViews () {
+        SpotifyService.playingState {
+            when(it) {
+                PlayingState.PLAYING -> showPauseButton()
+                PlayingState.STOPPED -> showPlayButton()
+                PlayingState.PAUSED -> showResumeButton()
             }
+        }
+    }
 
-            override fun onFailure(throwable: Throwable) {
-                Log.e("MainActivity", throwable.message, throwable)
-                // Something went wrong when attempting to connect! Handle errors here
-            }
-        })
+    private fun showPlayButton() {
+        playButton.visibility = View.VISIBLE
+    }
+
+    private fun showPauseButton() {
+        pauseButton.visibility = View.VISIBLE
+    }
+
+    private fun showResumeButton() {
+        resumeButton.visibility = View.VISIBLE
+    }
+
+
+
+    private fun setupListeners() {
+
+
+        playButton.setOnClickListener {
+            val connectionParams = ConnectionParams.Builder(clientId)
+                .setRedirectUri(redirectUri)
+                .showAuthView(true)
+                .build()
+
+            SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
+                override fun onConnected(appRemote: SpotifyAppRemote) {
+                    spotifyAppRemote = appRemote
+                    Log.d("MainActivity", "Connected! Yay!")
+                    // Now you can start interacting with App Remote
+                    connected()
+                }
+
+                override fun onFailure(throwable: Throwable) {
+                    Log.e("MainActivity", throwable.message, throwable)
+                    // Something went wrong when attempting to connect! Handle errors here
+                }
+            })
+            SpotifyService.play("spotify:playlist:19Zi93JmIGSaw0Gk57FajH")
+            showPauseButton()
+        }
+
+        pauseButton.setOnClickListener {
+            spotifyAppRemote.playerApi.pause()
+            showResumeButton()
+        }
+
+        resumeButton.setOnClickListener {
+            spotifyAppRemote.playerApi.resume()
+            showPauseButton()
+        }
     }
 
     private fun connected() {
